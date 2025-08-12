fe587e36fdfe03ca7299afa11070591341f6f61a
Ivan Maidurov <ivan.maidurov@omnevo.net>
2025-08-12 17:04:27 +0200

    deps(mockery): update mockery yaml and regenerate

--- module.go
+++ module.go
@@ -4,7 +4,7 @@ import (
 	"flamingo.me/dingo"
 )
 
-//go:generate go run github.com/vektra/mockery/v2@v3.5.2
+//go:generate go run github.com/vektra/mockery/v3@v3.5.2
 
 type (
 	// Module basic struct
